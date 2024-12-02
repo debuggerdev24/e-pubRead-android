222444f6f99c85f2176f9572b074672e491f1550
debuggerdev24 <debuggerdev.dds@gmail.com>
2024-12-02 17:32:54 +0530

    Initial commit

--- test-app/src/main/java/org/readium/r2/testapp/utils/ImportActivity.kt
+++ test-app/src/main/java/org/readium/r2/testapp/utils/ImportActivity.kt
@@ -1,13 +1,3 @@
-/*
- * Module: r2-testapp-kotlin
- * Developers: Aferdita Muriqi, Clément Baumann
- *
- * Copyright (c) 2018. European Digital Reading Lab. All rights reserved.
- * Licensed to the Readium Foundation under one or more contributor license agreements.
- * Use of this source code is governed by a BSD-style license which is detailed in the
- * LICENSE file present in the project repository where this source code is maintained.
- */
-
 package org.readium.r2.testapp.utils
 
 import android.app.Activity
@@ -32,7 +22,6 @@ class ImportActivity : Activity() {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
         startActivity(newIntent)
-
         finish()
     }
 
